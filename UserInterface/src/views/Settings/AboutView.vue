65c9a29d62f38c8bc0a89b24941addf8004c0dca
Tristan Jen <tristanjen@deci.com>
2026-08-13 17:37:44 +0800

    style: let AboutView fill the settings content area

--- UserInterface/src/views/Settings/AboutView.vue
+++ UserInterface/src/views/Settings/AboutView.vue
@@ -4,7 +4,7 @@ import { t } from '../../stores/locale'
 </script>
 
 <template>
-  <div class="flex justify-center items-center">
+  <div class="grow flex justify-center items-center">
     <span class="text-4xl font-bold">{{ t('common.underConstruction') }}</span>
   </div>
 </template>
